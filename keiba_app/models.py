33aa7f03de83e324c43125d346eb713b8d44e5fd
eiken <hideyoshi.noda@outlook.jp>
2024-04-17 07:31:33 +0900

    fix models.py(class horse)

--- keiba_app/models.py
+++ keiba_app/models.py
@@ -19,7 +19,8 @@ class Horse(models.Model):
     horse_name = models.CharField(max_length=12, verbose_name='馬名')
     recommend = models.IntegerField(default=0, verbose_name='おすすめ度')
     horse_age = models.IntegerField(default=2, verbose_name='馬齢')
-    description = models.TimeField(max_length==1000, null=True, verbose_name='説明')
+    description = models.TextField(max_length=1000, null=True, verbose_name='説明')
 
     def __str__(self):
         return self.name
+    
