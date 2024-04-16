885cdb37be28c047601aaad64052e5deaf9b661b
eiken <hideyoshi.noda@outlook.jp>
2024-04-17 07:16:05 +0900

    add models(class Horse)

--- keiba_app/models.py
+++ keiba_app/models.py
@@ -14,3 +14,12 @@ class Race(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Horse(models.Model):
+    horse_name = models.CharField(max_length=12, verbose_name='馬名')
+    recommend = models.IntegerField(default=0, verbose_name='おすすめ度')
+    horse_age = models.IntegerField(default=2, verbose_name='馬齢')
+    description = models.TimeField(max_length==1000, null=True, verbose_name='説明')
+
+    def __str__(self):
+        return self.name
